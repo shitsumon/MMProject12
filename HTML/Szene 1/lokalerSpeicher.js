cc038854fc761c82ffbf6b9c59b0924900849ab3
vendetta <vendetta@safetypost.de>
2012-11-06 19:18:48 +0100

    Kommentare hinzugefügt

--- HTML/Szene 1/lokalerSpeicher.js
+++ HTML/Szene 1/lokalerSpeicher.js
@@ -1,5 +1,6 @@
 function lokalerspeicher_start(){
 	
+	//erzeugt ein bild zur demonstration
 	var bild=new Image();
 	bild.onload=function(){
 		lokalerspeicher_speicherebild(this);
@@ -8,27 +9,30 @@ function lokalerspeicher_start(){
 }
 
 function lokalerspeicher_speicherebild(bild){
+	//erhält ein image object und speichert es als canvas dataurl (prinzipiell base64) in html5 localstorage
 	
+	//suche unsichtbaren canvas dummyspot
 	var canvas=document.getElementById("dummySpot");
 	var context=canvas.getContext("2d");
-
+	
+	//sichere original inhalt des canvas als dataurl
 	var original=canvas.toDataURL();
 	
 //	alert(original);
-	
+	//sichere abmessungen zur widerherstellung
 	var org_height=canvas.height, org_width=canvas.width;
-	
+	//lösche inhalt
 	context.clearRect(0,0,org_width,org_height);
-	
+	//setze abmessungen entsprechend des übergebenen bildes
 	canvas.height=bild.height;
 	canvas.width=bild.width;
-	
+	//zeichne das bild
 	context.drawImage(bild,0,0);
-	
+	//speicher dataurl des übergebenen bildes
 	var bild_url=canvas.toDataURL();
 	
 //	alert(bild_url);
-	
+	//stelle das original wieder her
 	canvas.height=org_height;
 	canvas.width=org_width;
 	
@@ -37,7 +41,7 @@ function lokalerspeicher_speicherebild(bild){
 		context.drawImage(this,0,0);
 		};
 	bild.src=original;
-	
+	//schreibe in localstorage
 	lokalerspeicher_speichere("bild",bild_url);
 }
 
